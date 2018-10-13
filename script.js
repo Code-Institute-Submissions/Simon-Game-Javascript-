5df8281c5cb76a46d0cf8681978355f708c7a6c6
Andrew Porritt <andrew.porritt@icloud.com>
2018-10-13 20:30:30 +0000

    initial ja and first loop test

---
+++ script.js
@@ -0,0 +1,22 @@
+// variables
+var correctVer = []
+var userVer = []
+var round = 2
+var maxRound = 20
+
+// Random number generator
+var lightFlash = Math.floor(Math.random() *5);
+
+// Create correctVer Array
+correctVer.push(lightFlash);
+// Check correctVer.length against round 
+if (correctVer.length == round) {
+    console.log (correctVer)
+} else {
+    console.log ("more lights please")
+}
+// correctVer to lights
+
+// correctVer to userVer
+
+// end - ourput Round
